ff201a4d0e1e54e64e2d87bbd2e7f222c1b2801b
Olena <shapovalev@gmail.com>
2023-12-04 13:19:07 +0100

    joi-changes

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -4,9 +4,12 @@ const addSchema = Joi.object({
   name: Joi.string()
     .required()
     .messages({ "any.required": "missing required name field" }),
-  email: Joi.string(),
-  phone: Joi.string(),
-  favorite: Joi.boolean(),
+  email: Joi.string()
+    .required()
+    .messages({ "any.required": "missing required name field" }),
+  phone: Joi.string()
+    .required()
+    .messages({ "any.required": "missing required name field" }),
 });
 
 const addFavoriteSchema = Joi.object({

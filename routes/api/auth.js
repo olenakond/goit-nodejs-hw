81d13214d3957c7ad21b3790886228340172589c
Olena <shapovalev@gmail.com>
2023-12-19 18:44:01 +0100

    verify

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,6 +7,10 @@ const router = express.Router();
 
 router.post("/register", validateBody(schemas.userSchema), ctrl.register);
 
+router.get("/verify/:verificationToken", ctrl.verifyEmail);
+
+router.post("/verify", validateBody(schemas.emailSchema), ctrl.resendVerifyEmail);
+
 router.post("/login", validateBody(schemas.userSchema), ctrl.login);
 
 router.get("/current", authenticate, ctrl.getCurrent);
